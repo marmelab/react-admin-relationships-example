0c80c7e97afc48345862b99361a353ce36b61a75
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-15 17:32:59 +0100

    add AuthorField

--- src/books/BookList.tsx
+++ src/books/BookList.tsx
@@ -2,16 +2,40 @@ import {
   Datagrid,
   List,
   NumberField,
-  ReferenceField,
   TextField,
+  useGetOne,
+  useRecordContext,
+  WrapperField,
 } from "react-admin";
+import { Typography } from "@mui/material";
+
+const AuthorField = () => {
+  const record = useRecordContext();
+  const {
+    data: author,
+    isPending,
+    error,
+  } = useGetOne(
+    "authors",
+    { id: record?.author_id },
+    { enabled: !!record?.author_id },
+  );
+  if (!record || isPending || error || !author) return null;
+  return (
+    <Typography variant="body2">
+      {author.first_name} {author.last_name}
+    </Typography>
+  );
+};
 
 export const BookList = () => (
   <List>
     <Datagrid>
       <TextField source="id" />
       <TextField source="title" />
-      <ReferenceField source="author_id" reference="authors" />
+      <WrapperField label="Author">
+        <AuthorField />
+      </WrapperField>
       <NumberField source="year" />
     </Datagrid>
   </List>
