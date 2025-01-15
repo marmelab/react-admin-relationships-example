c6ad103a331920d5bddcfa58490284477c145ef8
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-15 17:43:12 +0100

    add link to Author Show

--- src/books/BookList.tsx
+++ src/books/BookList.tsx
@@ -5,9 +5,11 @@ import {
   TextField,
   useGetOne,
   useRecordContext,
+  useCreatePath,
   WrapperField,
 } from "react-admin";
 import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const AuthorField = () => {
   const record = useRecordContext();
@@ -20,9 +22,26 @@ const AuthorField = () => {
     { id: record?.author_id },
     { enabled: !!record?.author_id },
   );
+
+  const createPath = useCreatePath();
+  const authorShowPath = createPath({
+    resource: "authors",
+    id: record?.author_id,
+    type: "show",
+  });
+  // to prevent click bubbling in a datagrid with rowClick
+  const stopPropagation = (e: React.MouseEvent) => e.stopPropagation();
+
   if (!record || isPending || error || !author) return null;
+
   return (
-    <Typography variant="body2">
+    <Typography
+      variant="body2"
+      component={Link}
+      to={authorShowPath}
+      onClick={stopPropagation}
+      color="primary"
+    >
       {author.first_name} {author.last_name}
     </Typography>
   );
