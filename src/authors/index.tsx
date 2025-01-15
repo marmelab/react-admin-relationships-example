2672a16e950648030207996cfa5b18aa25bd6952
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-15 17:48:37 +0100

    replace AuthorField with ReferenceField and recordRepresentation

--- src/authors/index.tsx
+++ src/authors/index.tsx
@@ -10,6 +10,7 @@ const authors: ResourceProps = {
   list: ListGuesser,
   edit: EditGuesser,
   show: ShowGuesser,
+  recordRepresentation: (author) => `${author.first_name} ${author.last_name}`,
 };
 
 export default authors;
--- src/books/BookList.tsx
+++ src/books/BookList.tsx
@@ -3,58 +3,15 @@ import {
   List,
   NumberField,
   TextField,
-  useGetOne,
-  useRecordContext,
-  useCreatePath,
-  WrapperField,
+  ReferenceField,
 } from "react-admin";
-import { Typography } from "@mui/material";
-import { Link } from "react-router-dom";
-
-const AuthorField = () => {
-  const record = useRecordContext();
-  const {
-    data: author,
-    isPending,
-    error,
-  } = useGetOne(
-    "authors",
-    { id: record?.author_id },
-    { enabled: !!record?.author_id },
-  );
-
-  const createPath = useCreatePath();
-  const authorShowPath = createPath({
-    resource: "authors",
-    id: record?.author_id,
-    type: "show",
-  });
-  // to prevent click bubbling in a datagrid with rowClick
-  const stopPropagation = (e: React.MouseEvent) => e.stopPropagation();
-
-  if (!record || isPending || error || !author) return null;
-
-  return (
-    <Typography
-      variant="body2"
-      component={Link}
-      to={authorShowPath}
-      onClick={stopPropagation}
-      color="primary"
-    >
-      {author.first_name} {author.last_name}
-    </Typography>
-  );
-};
 
 export const BookList = () => (
   <List>
     <Datagrid>
       <TextField source="id" />
       <TextField source="title" />
-      <WrapperField label="Author">
-        <AuthorField />
-      </WrapperField>
+      <ReferenceField source="author_id" reference="authors" link="show" />
       <NumberField source="year" />
     </Datagrid>
   </List>
