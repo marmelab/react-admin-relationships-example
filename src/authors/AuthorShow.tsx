05e7444841ed085fd2eebe82a220c2e922c70e86
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-17 16:12:48 +0100

    add Books by this author

--- src/authors/AuthorShow.tsx
+++ src/authors/AuthorShow.tsx
@@ -1,12 +1,56 @@
-import { DateField, Show, SimpleShowLayout, TextField } from "react-admin";
+import {
+  Datagrid,
+  DateField,
+  Labeled,
+  ListContextProvider,
+  NumberField,
+  ReferenceField,
+  ResourceContextProvider,
+  Show,
+  SimpleShowLayout,
+  TextField,
+  useGetList,
+  useList,
+} from "react-admin";
+import { useParams } from "react-router-dom";
 
-export const AuthorShow = () => (
-  <Show>
-    <SimpleShowLayout>
-      <TextField source="id" />
-      <TextField source="first_name" />
-      <TextField source="last_name" />
-      <DateField source="date_of_birth" />
-    </SimpleShowLayout>
-  </Show>
-);
+export const AuthorShow = () => {
+  const { id: author_id } = useParams<"id">();
+  const {
+    data: books,
+    error,
+    isPending,
+  } = useGetList(
+    "books",
+    { filter: { author_id }, pagination: { page: 1, perPage: 100 } },
+    { enabled: !!author_id },
+  );
+  const booksListContext = useList({ data: books, error, isPending });
+
+  return (
+    <Show>
+      <SimpleShowLayout>
+        <TextField source="id" />
+        <TextField source="first_name" />
+        <TextField source="last_name" />
+        <DateField source="date_of_birth" />
+        <Labeled label="Books by this author">
+          <ResourceContextProvider value="books">
+            <ListContextProvider value={booksListContext}>
+              <Datagrid bulkActionButtons={false} rowClick="show">
+                <TextField source="id" />
+                <TextField source="title" />
+                <ReferenceField
+                  source="author_id"
+                  reference="authors"
+                  link="show"
+                />
+                <NumberField source="year" />
+              </Datagrid>
+            </ListContextProvider>
+          </ResourceContextProvider>
+        </Labeled>
+      </SimpleShowLayout>
+    </Show>
+  );
+};
