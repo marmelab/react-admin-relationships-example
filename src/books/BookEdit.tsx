d3664d69211277cbfffa1fd3fbfb7d251c268d96
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-17 19:16:30 +0100

    AuthorAutocompleteInput server-side filter

--- src/books/BookEdit.tsx
+++ src/books/BookEdit.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useState } from "react";
 import {
   AutocompleteInput,
   Edit,
@@ -5,22 +6,80 @@ import {
   SimpleForm,
   TextInput,
   useGetList,
+  useGetOne,
+  useRecordContext,
 } from "react-admin";
+import { AutocompleteInputChangeReason } from "@mui/material";
+import { keepPreviousData } from "@tanstack/react-query";
+import debounce from "lodash/debounce";
 
 const AuthorAutocompleteInput = () => {
+  // Fetch the currently selected author's data
+  const book = useRecordContext();
   const {
-    data: authors,
+    data: currentAuthor,
     error,
     isPending,
-  } = useGetList("authors", {
-    pagination: { page: 1, perPage: 100 },
-  });
-  if (error || isPending) return null;
+  } = useGetOne(
+    "authors",
+    { id: book?.author_id },
+    { enabled: !!book?.author_id },
+  );
+
+  // Fetch the (filtered) list of possible authors
+  const [authorFilter, setAuthorFilter] = useState<string | undefined>(
+    undefined,
+  );
+  const { data: authors } = useGetList(
+    "authors",
+    {
+      pagination: { page: 1, perPage: 100 },
+      filter: { q: authorFilter },
+    },
+    // keep previous data as placeholder data to avoid clearing the input
+    // each time the filter changes
+    { placeholderData: keepPreviousData },
+  );
+
+  // (Debounced) update the filter on input change
+  const onInputChange = debounce(
+    (
+      _event: React.SyntheticEvent,
+      value: string,
+      reason: AutocompleteInputChangeReason,
+    ) => {
+      if (reason === "input") {
+        setAuthorFilter(value);
+      }
+      if (reason === "reset" || reason === "clear") {
+        setAuthorFilter(undefined);
+      }
+    },
+    500,
+  );
+
+  // Add the current author to the list of choices if not already present
+  const choices = useMemo(() => {
+    if (!authors) return authors;
+    if (authors?.some((author) => author.id === currentAuthor?.id)) {
+      return authors;
+    }
+    return [...authors, currentAuthor];
+  }, [authors, currentAuthor]);
+
+  // Memoized optionText
+  const optionText = useCallback(
+    (author) => `${author.first_name} ${author.last_name}`,
+    [],
+  );
+
+  if (error || isPending || !authors) return null;
   return (
     <AutocompleteInput
       source="author_id"
-      choices={authors}
-      optionText={(author) => `${author.first_name} ${author.last_name}`}
+      choices={choices}
+      optionText={optionText}
+      onInputChange={onInputChange}
     />
   );
 };
