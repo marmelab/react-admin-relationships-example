56a0e4a5bb7c37055f75d2414a2085301d67d209
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-20 15:11:05 +0100

    add ReferenceManyToManyInput to BookEdit

--- src/books/BookEdit.tsx
+++ src/books/BookEdit.tsx
@@ -4,7 +4,9 @@ import {
   ReferenceInput,
   SimpleForm,
   TextInput,
+  AutocompleteArrayInput,
 } from "react-admin";
+import { ReferenceManyToManyInput } from "@react-admin/ra-relationships";
 
 export const BookEdit = () => (
   <Edit>
@@ -13,6 +15,13 @@ export const BookEdit = () => (
       <TextInput source="title" />
       <ReferenceInput source="author_id" reference="authors" />
       <NumberInput source="year" />
+      <ReferenceManyToManyInput
+        reference="collections"
+        through="books_collections"
+        using="book_id,collection_id"
+      >
+        <AutocompleteArrayInput label="Collections" optionText="name" />
+      </ReferenceManyToManyInput>
     </SimpleForm>
   </Edit>
 );
