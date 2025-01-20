33ddc4980e59a4740283f7aa4ea3878e2f49c286
Jean-Baptiste Kaiser <jb@marmelab.com>
2025-01-20 13:43:00 +0100

    use ReferenceManyInput in AuthorEdit

--- src/authors/AuthorEdit.tsx
+++ src/authors/AuthorEdit.tsx
@@ -1,12 +1,26 @@
-import { DateInput, Edit, SimpleForm, TextInput } from "react-admin";
+import {
+  DateInput,
+  Edit,
+  SimpleForm,
+  TextInput,
+  SimpleFormIterator,
+  NumberInput,
+} from "react-admin";
+import { ReferenceManyInput } from "@react-admin/ra-relationships";
 
 export const AuthorEdit = () => (
-  <Edit>
+  <Edit mutationMode="optimistic">
     <SimpleForm>
       <TextInput source="id" />
       <TextInput source="first_name" />
       <TextInput source="last_name" />
       <DateInput source="date_of_birth" />
+      <ReferenceManyInput reference="books" target="author_id">
+        <SimpleFormIterator inline disableReordering>
+          <TextInput source="title" />
+          <NumberInput source="year" />
+        </SimpleFormIterator>
+      </ReferenceManyInput>
     </SimpleForm>
   </Edit>
 );
